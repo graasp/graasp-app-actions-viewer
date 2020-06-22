2218309dd30b6adee6e12f692f24d7946bf7251d
Hassan Abdul Ghaffar <hasan.agh96@gmail.com>
2020-06-22 17:59:39 +0300

    feat: add chart with sample daily number of actions
    adds a chart with sample daily number of actions
    
    close #1

--- src/config/formats.js
+++ src/config/formats.js
@@ -1,2 +1,3 @@
 export const ACTION_FORMAT = 'v1';
 export const APP_INSTANCE_RESOURCE_FORMAT = 'v1';
+export const DATE_FROMAT = 'DD/MM/YYYY';
---
+++ src/config/styles.js
@@ -0,0 +1,3 @@
+// todo: remove suppression on multiple imports
+// eslint-disable-next-line import/prefer-default-export
+export const NIVO_LINE_MARGIN = { top: 50, right: 110, bottom: 50, left: 60 };
